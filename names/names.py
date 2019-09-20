fc950e6db1ed406c36daeb3f512b9b02a5585a50
josh182014 <josh182014@gmail.com>
2019-09-20 10:16:33 -0600

    switched duplicates to a one liner

--- names/names.py
+++ names/names.py
@@ -10,14 +10,7 @@ f = open('names_2.txt', 'r')
 names_2 = set(f.read().split("\n"))  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    if name_1 in names_2:
-        duplicates.append(name_1)
-
-# with open('names_1.txt', 'r') as n1:
-#     duplicates.append(part in words for line in file for part in line.split())
-
+duplicates = [name_1 for name_1 in names_1 if name_1 in names_2]
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
