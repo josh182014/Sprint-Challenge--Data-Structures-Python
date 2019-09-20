2deaa83286680ea65d6f32b0422aa05ac7505612
josh182014 <josh182014@gmail.com>
2019-09-20 10:50:26 -0600

    added stretch to names.py

--- names/names.py
+++ names/names.py
@@ -1,5 +1,6 @@
 import time
 
+# MVP
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -8,6 +9,24 @@ f.close()
 
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+duplicates = [name_1 for name_1 in names_1 if name_1 in names_2]
+
+end_time = time.time()
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
+
+
+# STRETCH
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = set(f.read().split("\n"))  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = set(f.read().split("\n"))  # List containing 10000 names
 f.close()
 
 duplicates = [name_1 for name_1 in names_1 if name_1 in names_2]
