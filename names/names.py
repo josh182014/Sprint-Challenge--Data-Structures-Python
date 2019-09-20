34eef400b72c7fb16ac41960f2a6cb3af21a2298
josh182014 <josh182014@gmail.com>
2019-09-20 10:02:59 -0600

    got names to run slightly faster but still not under 1 second

--- names/names.py
+++ names/names.py
@@ -12,11 +12,9 @@ f.close()
 
 duplicates = []
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    if name_1 in names_2:
+        duplicates.append(name_1)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
