4d65ffc0ceb7a2be0658e22be56cd558e084bbd2
josh182014 <josh182014@gmail.com>
2019-09-20 10:13:14 -0600

    switched from using lists to using sets to make comparison much faster

--- names/names.py
+++ names/names.py
@@ -3,17 +3,21 @@ import time
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
+names_1 = set(f.read().split("\n"))  # List containing 10000 names
 f.close()
 
 f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
+names_2 = set(f.read().split("\n"))  # List containing 10000 names
 f.close()
 
 duplicates = []
 for name_1 in names_1:
     if name_1 in names_2:
         duplicates.append(name_1)
+
+# with open('names_1.txt', 'r') as n1:
+#     duplicates.append(part in words for line in file for part in line.split())
+
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
